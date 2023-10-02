783eff84147056d4a5d49cb4db730fe51d14bef8
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-10-02 21:52:13 +0600

    limitation

---
+++ Class-2/limitation.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    
+    int a = 100000;
+    long long int b = 100000000;
+    printf("%lld,a");
+    return 0;
+}
---
+++ Class-2/output.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    // int rahim;
+    // char c;
+    // float f;
+
+    // scanf("%d %f %c",&rahim,&f ,&c);
+    // printf("%d %0.1f",rahim,f,c);
+    int a,b;
+    scanf("%d%% %d%%",&a,&b);
+    printf("%d%% %d%%",a,b);
+    return 0;
+}
