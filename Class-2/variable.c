b91ebd067b5fad13c0fcc5d476b6cfcbc614ce33
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-10-02 21:09:39 +0600

    int float char

--- Class-2/variable.c
+++ Class-2/variable.c
@@ -1,6 +1,17 @@
 #include<stdio.h>
 int main(){
-    int mehedi = 12 , karim=200;
-    printf("%d %d", karim,mehedi);
+    // int mehedi = 12 , karim=200;
+    // printf("%d %d", karim,mehedi);
+
+    // float chol = 2.5;
+    // printf("%f", chol);
+
+    char vlo = 'K';
+    printf("%c", vlo);
     return 0;
 }
+
+// data type print system
+// %d = int
+// %f = float
+// %c = char
