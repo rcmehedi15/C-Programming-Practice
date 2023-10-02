e67a7b212b8ac57b943a27b123b7e4769cc908f8
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-10-02 20:55:34 +0600

    variable print

---
+++ Class-2/print.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+int main()
+{
+    printf("Hello \nI am a Programmer ");
+    
+    return 0;
+}
+
+// Hi I am a Programmer 
+
+// Special Characters 
+// \n - new line
+// \t - tab
+// \\ - \ 
+// %% - %
---
+++ Class-2/variable.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    int mehedi = 12 , karim=200;
+    printf("%d %d", karim,mehedi);
+    return 0;
+}
