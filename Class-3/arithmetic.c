478502e1425ef467aebe37e5ae1c278664bc2a18
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-10-03 23:33:47 +0600

    3 days

---
+++ Class-3/arithmetic.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a = 10,b=30;
+    int sum = a+b;
+    printf("%d",sum);
+    return 0;
+}
+// arithmetic operator 
+// + - * / 
---
+++ Class-3/conditional.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int tk;
+    scanf("%d ", &tk);
+    if(tk == 100)
+    {
+        printf("Burger Khabo");
+
+    }
+    else {
+        printf("saradin kichu khabo na");
+    }
+    return 0;
+}
+
+//  coditional opereator 
+
+// == ,  < > , != ,>=
