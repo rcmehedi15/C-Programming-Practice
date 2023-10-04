bd309ec21ab5b1b54a72145f0c0a2191b5a70964
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-10-04 21:11:26 +0600

    conditional 3

--- Class-3/arithmetic.c
+++ Class-3/arithmetic.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int a = 10,b=30;
-    int sum = a+b;
-    printf("%d",sum);
+    // int a = 10,b=30;
+    // int sum = a+b;
+    // printf("%d",sum);
+
+    int a = 3;
+    int b = 20;
+    int mod = a%b;
+    printf("%d",mod);
     return 0;
 }
 // arithmetic operator 
-// + - * / 
+// + - * / %
